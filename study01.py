95bff5b8c7cbf06c2610f071394a6fd91b94b775
jaybon1 <ponggas2@gmail.com>
2022-10-21 17:46:21 +0900

    표준 타입형

--- study01.py
+++ study01.py
@@ -1,6 +1,82 @@
-name = "jaybon"
 
-print(name)
+char_list = [a for a in "가나다라마바사"]
 
-# git
-# 터미널
+print(char_list)
+
+
+
+# my_tuple = (1 ,1,   2   ,3,   4)
+
+# my_tuple = [1,2,3]
+
+# print(my_tuple)
+
+# for item in my_tuple:
+#     print(item)
+
+
+
+
+# my_dict = {"name" : "jaybon", "age" : 12}
+
+# for key, value in my_dict.items():
+    # print(f"{key} 은/는 {value}")
+
+
+
+
+# my_set = {1,2,3,4,1,1,1,1}
+# my_set = set([1,2,3,4,1,1,1,1])
+
+# print(my_set)
+
+# for item in my_set:
+#     print(item)
+
+
+
+# print(True or False)
+
+# result = not (1 != 2)
+
+# print(result)
+
+
+# print(id(None))
+
+# print(1)
+
+# def sum_two_num(n1, n2):
+#     return n1 + n2
+
+# sum_num = sum_two_num(1, 2)
+# sum_num = 3
+
+# print(sum_num)
+
+
+
+
+
+# def check_data():
+#     data = int(input())
+
+#     if data == 1:
+#         print("1이다")
+#     elif data == 2:
+#         print("2다")
+#     elif data == 3:
+#         print("3다")
+#     elif data == 4:
+#         print("4다")
+#     else:
+#         print("모르겠다")
+        
+
+# while True:
+#     check_data()
+
+
+
+
+
