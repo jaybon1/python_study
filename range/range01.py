eb48d7147eb7259d102f28954d83fddda73cd5f5
jaybon1 <ponggas2@gmail.com>
2022-10-22 14:14:15 +0900

    range

---
+++ range/range01.py
@@ -0,0 +1,13 @@
+temp = list(range(3, 30, 3))
+
+# print(temp)
+
+temp = list(range(0, 10))
+
+# for item in temp:
+#     print("안녕")
+
+# temp = [item for item in range(0, 10)]
+temp = ["안녕" for item in range(0, 10)]
+
+print(temp)
